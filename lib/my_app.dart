f71b2428b4bf09b89c80228149875600c8e67072
Mikael David <63308965+MikaelDDavidd@users.noreply.github.com>
2023-04-03 12:44:56 -0300

    my appd update
    safe area

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -1,18 +1,17 @@
 import 'package:fala_ai_app/screens/home_page.dart';
-import 'package:fala_ai_app/screens/splash_screen.dart';
 import 'package:flutter/material.dart';
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Fala Aí',
-      routes: {
-        '/': (context) => const SplashScreen(),
-      },
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false, // remove a barra de depuração
+      home: SafeArea(
+        // ajusta a área de visualização abaixo da área de notificação
+        child: HomePage(),
+      ),
     );
   }
 }
