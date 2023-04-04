7048267eb6b401a6ef204374cb5baac7af9586fc
Mikael David <63308965+MikaelDDavidd@users.noreply.github.com>
2023-04-04 19:33:48 -0300

    refiz denovo

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,26 +1,15 @@
-import 'package:fala_ai_app/models/character_model.dart';
+class Constants {
+  static const String appName = 'Fala Aí';
 
-final List<String> categories = [
-  'Anime',
-  'Desenho animado',
-];
+  static const List<String> categories = [
+    'Categoria 1',
+    'Categoria 2',
+    'Categoria 3',
+  ];
 
-final List<CharacterModel> animeCharacters = [
-  CharacterModel(
-    name: 'Goku',
-    imageUrl:
-        'https://cdn.falaai.com.br/uploads/avatar/avatar_1_8bd0246cb0.png',
-    characterDetails:
-        'Goku é o protagonista da franquia Dragon Ball. Ele é excepcionalmente forte, com uma força sobre-humana e habilidades de luta incríveis. Goku é também conhecido por sua personalidade simples e honesta, bem como por sua fome insaciável. ',
-  ),
-];
-
-final List<CharacterModel> cartoonCharacters = [
-  CharacterModel(
-    name: 'Homer Simpson',
-    imageUrl:
-        'https://cdn.falaai.com.br/uploads/avatar/avatar_4_4a4a4b7a90.png',
-    characterDetails:
-        'Homer Simpson é o pai de família da família Simpson. Ele é um trabalhador preguiçoso, geralmente ignorante e incompetente, mas é um bom marido e pai amoroso. Ele também é conhecido por sua paixão por comida e cerveja. ',
-  ),
-];
+  static const Map<String, List<String>> characters = {
+    'Categoria 1': ['Personagem 1', 'Personagem 2', 'Personagem 3'],
+    'Categoria 2': ['Personagem 4', 'Personagem 5', 'Personagem 6'],
+    'Categoria 3': ['Personagem 7', 'Personagem 8', 'Personagem 9'],
+  };
+}
--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,102 +1,85 @@
-// ignore_for_file: library_private_types_in_public_api
-
 import 'package:flutter/material.dart';
-import 'package:fala_ai_app/models/character_model.dart';
-import 'package:fala_ai_app/widgets/selected_character_card.dart';
 import 'package:fala_ai_app/constants.dart';
 
 class HomePage extends StatefulWidget {
-  const HomePage({Key? key}) : super(key: key);
+  const HomePage({super.key});
 
   @override
   _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-  late String _selectedCategory;
-  late CharacterModel _selectedCharacter;
-  late String _characterDetails;
-
-  @override
-  void initState() {
-    super.initState();
-    _selectedCategory = categories.first;
-    _selectedCharacter = characters[_selectedCategory]![0];
-    _characterDetails = _selectedCharacter.characterDetails;
-  }
-
-  void _onCategoryChanged(String category) {
-    setState(() {
-      _selectedCategory = category;
-      _selectedCharacter = characters[_selectedCategory]![0];
-      _characterDetails = _selectedCharacter.characterDetails;
-    });
-  }
-
-  void _onCharacterChanged(CharacterModel character) {
-    setState(() {
-      _selectedCharacter = character;
-      _characterDetails = _selectedCharacter.characterDetails;
-    });
-  }
-
-  Widget _buildCategoryDropdown() {
-    return DropdownButton<String>(
-      value: _selectedCategory,
-      items: categories.map((category) {
-        return DropdownMenuItem<String>(
-          value: category,
-          child: Text(category),
-        );
-      }).toList(),
-      onChanged: _onCategoryChanged,
-    );
-  }
-
-  Widget _buildCharacterDropdown() {
-    return DropdownButton<CharacterModel>(
-      value: _selectedCharacter,
-      items: characters[_selectedCategory]!.map((character) {
-        return DropdownMenuItem<CharacterModel>(
-          value: character,
-          child: Text(character.name),
-        );
-      }).toList(),
-      onChanged: _onCharacterChanged,
-    );
-  }
-
-  Widget _buildCharacterCard() {
-    final character = _selectedCharacter;
-    return SelectedCharacterCard(
-      name: character.name,
-      imageUrl: character.imageUrl,
-      characterDetails: character.characterDetails,
-    );
-  }
+  String _selectedCategory;
+  String _selectedCharacter;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              const Text(
-                'Fala aí!',
-                style: TextStyle(fontSize: 36.0, fontWeight: FontWeight.bold),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              Constants.appName,
+              style: TextStyle(
+                fontSize: 36.0,
+                fontWeight: FontWeight.bold,
               ),
-              const SizedBox(height: 16.0),
-              _buildCategoryDropdown(),
-              const SizedBox(height: 16.0),
-              _buildCharacterDropdown(),
-              const SizedBox(height: 16.0),
-              _buildCharacterCard(),
-            ],
-          ),
+            ),
+            const SizedBox(height: 24.0),
+            DropdownButtonFormField(
+              decoration: const InputDecoration(
+                labelText: 'Categoria',
+                border: OutlineInputBorder(),
+              ),
+              value: _selectedCategory,
+              items: Constants.categories.map((category) {
+                return DropdownMenuItem(
+                  child: Text(category),
+                  value: category,
+                );
+              }).toList(),
+              onChanged: (category) {
+                setState(() {
+                  _selectedCategory = category;
+                  _selectedCharacter = null;
+                });
+              },
+            ),
+            const SizedBox(height: 16.0),
+            DropdownButtonFormField(
+              decoration: const InputDecoration(
+                labelText: 'Personagem',
+                border: OutlineInputBorder(),
+              ),
+              value: _selectedCharacter,
+              items: Constants.characters[_selectedCategory].map((character) {
+                return DropdownMenuItem(
+                  child: Text(character),
+                  value: character,
+                );
+              }).toList(),
+              onChanged: (character) {
+                setState(() {
+                  _selectedCharacter = character;
+                });
+              },
+            ),
+            const Spacer(),
+            PopupMenuButton(
+              itemBuilder: (BuildContext context) {
+                return [
+                  const PopupMenuItem(
+                    child: Text('Sobre o App'),
+                  ),
+                  const PopupMenuItem(
+                    child: Text('Avaliar o App'),
+                  ),
+                ];
+              },
+              icon: const Icon(Icons.more_vert),
+            ),
+          ],
         ),
       ),
     );
