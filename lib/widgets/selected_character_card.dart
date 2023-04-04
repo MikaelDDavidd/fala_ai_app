85b80600e7a48e84dc0996c6c3b9e43d737998e4
Mikael David <63308965+MikaelDDavidd@users.noreply.github.com>
2023-04-04 14:56:34 -0300

    Update selected_character_card.dart

--- lib/widgets/selected_character_card.dart
+++ lib/widgets/selected_character_card.dart
@@ -2,9 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:fala_ai_app/models/character_model.dart';
 
 class SelectedCharacterCard extends StatelessWidget {
+  const SelectedCharacterCard({Key? key, required this.character})
+      : super(key: key);
   final Character character;
-
-  const SelectedCharacterCard({required this.character});
 
   @override
   Widget build(BuildContext context) {
