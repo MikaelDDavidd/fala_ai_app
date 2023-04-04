e1b4cb4ecf224a43ae8569a72ff2f2ef17db6e26
Mikael David <63308965+MikaelDDavidd@users.noreply.github.com>
2023-04-04 14:51:51 -0300

    Update constants.dart

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,37 +1,45 @@
-class Constants {
-  static const List<String> categories = [
-    "Comics",
-    "Movies",
-  ];
+import 'package:flutter/material.dart';
 
-  static const Map<String, List<Map<String, String>>> characters = {
-    "Comics": [
-      {
-        "name": "Spider-Man",
-        "imageUrl": "https://i.imgur.com/yW2W9hD.png",
-        "details":
-            "Peter Parker was bitten by a radioactive spider and gained the abilities of a spider, such as super strength, agility, and the ability to climb walls.",
-      },
-      {
-        "name": "Iron Man",
-        "imageUrl": "https://i.imgur.com/8F2gsDm.png",
-        "details":
-            "Tony Stark created a suit of powered armor that gives him superhuman strength and the ability to fly. He uses his suit to fight crime and protect the world.",
-      },
-    ],
-    "Movies": [
-      {
-        "name": "Captain America",
-        "imageUrl": "https://i.imgur.com/Fa3q4F7.png",
-        "details":
-            "Steve Rogers was a frail young man who was transformed into a super soldier by a secret serum. He fights for the American ideals of freedom and justice as Captain America.",
-      },
-      {
-        "name": "Thor",
-        "imageUrl": "https://i.imgur.com/2NLO16j.png",
-        "details":
-            "Thor is the Asgardian god of thunder, and possesses immense physical strength, durability, and the ability to summon lightning at will.",
-      },
-    ],
-  };
+class Character {
+  final String name;
+  final String imagePath;
+  final String description;
+  final String categories;
+
+  const Character({
+    required this.name,
+    required this.imagePath,
+    required this.description,
+    required this.categories,
+  });
 }
+
+const List<Character> dcCharacters = [
+  Character(
+    name: 'Superman',
+    imagePath:
+        'https://www.dccomics.com/sites/default/files/styles/character_thumb_160x160/public/CharThumb_215x215_SUPERMAN_57b16f43d3f3c3.10580054.jpg?itok=FySvkj3M',
+    description:
+        'Superman is a fictional superhero. The character was created by writer Jerry Siegel and artist Joe Shuster, and first appeared in Action Comics #1, a comic book published on April 18, 1938.',
+    categories: 'DC',
+  ),
+  Character(
+    name: 'Batman',
+    imagePath:
+        'https://www.dccomics.com/sites/default/files/styles/character_thumb_160x160/public/CharThumb_215x215_BATMAN_57b16f41836ae9.43122231.jpg?itok=XOCgYyjQ',
+    description:
+        'Batman is a fictional superhero. The character was created by artist Bob Kane and writer Bill Finger, and first appeared in Detective Comics #27, a comic book published on March 30, 1939.',
+    categories: 'DC',
+  ),
+];
+
+const List<Character> marvelCharacters = [
+  Character(
+    name: 'Iron Man',
+    imagePath:
+        'https://terrigen-cdn-dev.marvel.com/content/prod/1x/002irm_ons_crd_03.jpg',
+    description:
+        'Batman is a fictional superhero. The character was created by artist Bob Kane and writer Bill Finger, and first appeared in Detective Comics #27, a comic book published on March 30, 1939.',
+    categories: 'Marvel',
+  ),
+];
